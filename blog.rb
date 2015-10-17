587b7efc66aa3029955a11e204b9e393e24cb1cd
natalia <nataliav@gmail.com>
2015-10-17 15:38:51 +0200

    Separated code into files

--- blog.rb
+++ blog.rb
@@ -1,18 +1,4 @@
-require 'date'
-require 'colorize'
-require 'pry'
 
-
-class Post
-	attr_accessor :title, :date, :text, :sponsored
-
-	def initialize title, date, text, sponsored = false
-		@title = title
-		@date = date
-		@text = text
-		@sponsored = sponsored
-	end
-end
 
 class Blog
 
@@ -30,7 +16,7 @@ class Blog
 	def compose_post post
 
 		post_var = ""
-		
+
 		post_var += post.sponsored ? "******#{post.title}******".colorize(:red) : post.title
 
 		post_var += "\n**************\n#{post.text}\n----------------\n"
@@ -44,26 +30,26 @@ class Blog
 	def create_front_page front_page, page_num
 		front_page_text =""
 		offset = (page_num - 1) * @posts_per_page
-		(offset..(offset+posts_per_page-1)).each do |post| 
+		(offset..(offset+posts_per_page-1)).each do |post|
 			#binding.pry
 			front_page_text += compose_post(front_page[post]) unless post >= front_page.count
-		end 
+		end
 		front_page_text
-		
+
 	end
 
 	def print_pagination num_pages, current_page
 		puts ""
 		(1..num_pages).each do |p|
-			if p == current_page 
+			if p == current_page
 				print " #{p} ".colorize(:red)
-			else 
+			else
 				print " #{p} "
 			end
 		end
 		puts "\n"
-		
-	end 
+
+	end
 
 	def display_page(front_page, number_pages, page)
 		front_page_text = create_front_page(front_page, page)
@@ -83,28 +69,15 @@ class Blog
 				display_page(front_page, number_pages, page.to_i)
 			when 'q'
 				puts "Good bye!"
-			else 
+			else
 				puts "Incorrect number."
 			end
 			print "Type the page number you want to go to, q to quit: "
 			page = gets.chomp
-		end 
+		end
 
 	end
 
 end
 
 
-blog = Blog.new 4
-
-blog.add_post Post.new("Post title 1", DateTime.now, "Post 1 text")
-blog.add_post Post.new("Post title 2", DateTime.now,  "Post 2 text", true)
-blog.add_post Post.new("Post title 3", DateTime.now,  "Post 3 text")
-blog.add_post Post.new("Post title 4", DateTime.now,  "Post 4 text")
-blog.add_post Post.new("Post title 5", DateTime.now,  "Post 5 text", true)
-blog.add_post Post.new("Post title 6", DateTime.now,  "Post 6 text")
-blog.add_post Post.new("Post title 7", DateTime.now,  "Post 7 text")
-blog.add_post Post.new("Post title 8", DateTime.now,  "Post 8 text")
-blog.add_post Post.new("Post title 9", DateTime.now,  "Post 9 text")
-
-blog.publish_front_page
